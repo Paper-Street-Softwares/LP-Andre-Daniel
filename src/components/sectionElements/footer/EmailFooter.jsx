a31da75880851c063f33a87caaa98305ca7797ac
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-15 10:06:23 -0300

    fix: Altera e-mail

--- src/components/sectionElements/footer/EmailFooter.jsx
+++ src/components/sectionElements/footer/EmailFooter.jsx
@@ -11,22 +11,20 @@ function EmailFooter({
 }) {
   return (
     <>
-      {/* <MotionDivDownToUp>
+      <MotionDivDownToUp>
         <div className="flex full gap-x-[12px] items-center opacity-90">
           <Mail />
 
           <p className="text-paragraph2">{content.texts.infos.email}</p>
         </div>
-      </MotionDivDownToUp> */}
+      </MotionDivDownToUp>
 
       {emailTwo && (
         <MotionDivDownToUp>
           <div className="flex full gap-x-[12px] items-center opacity-90">
             <Mail />
 
-            <p className="text-paragraph2">
-              herculesrocha.machado.adv@gmail.com{" "}
-            </p>
+            <p className="text-paragraph2">A_Definir</p>
           </div>
         </MotionDivDownToUp>
       )}
