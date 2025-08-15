1a80825d2b1ae55e3b10a723fefe6d1ceec527c9
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-15 10:10:07 -0300

    fix: email alterado conforme doc

--- src/components/sectionElements/footer/EmailFooter.jsx
+++ src/components/sectionElements/footer/EmailFooter.jsx
@@ -11,20 +11,20 @@ function EmailFooter({
 }) {
   return (
     <>
-      <MotionDivDownToUp>
+      {/* <MotionDivDownToUp>
         <div className="flex full gap-x-[12px] items-center opacity-90">
           <Mail />
 
           <p className="text-paragraph2">{content.texts.infos.email}</p>
         </div>
-      </MotionDivDownToUp>
+      </MotionDivDownToUp> */}
 
       {emailTwo && (
         <MotionDivDownToUp>
           <div className="flex full gap-x-[12px] items-center opacity-90">
             <Mail />
 
-            <p className="text-paragraph2">A_Definir</p>
+            <p className="text-paragraph2">{content.texts.infos.emailSecundario}</p>
           </div>
         </MotionDivDownToUp>
       )}
